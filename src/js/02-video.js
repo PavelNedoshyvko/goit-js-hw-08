986ac0107b816726736f3a69cc39a45c750f5757
Pavel Nedoshyvko <plight83@gmail.com>
2023-08-07 17:31:30 +0300

    added 02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
+    const player = new Player(iframe);
+
+player.on('timeupdate', throttle(recordingCurrentTime, 1000));
+	
+function recordingCurrentTime(data) { 
+	localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+}
+
+try {
+	const videoplayerCurrentTime = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+	player.setCurrentTime(videoplayerCurrentTime.seconds);
+} catch (error) {
+	console.log(error.name);
+	console.log(error.message);
+};
