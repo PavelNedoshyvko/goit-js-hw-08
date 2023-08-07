616fe7a70e52c7d4f4cf4d9ce054b7e3b7e69398
Pavel Nedoshyvko <plight83@gmail.com>
2023-08-07 13:18:36 +0300

    added 01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const refs = {
+	container: document.querySelector('.gallery'),
+};
+
+createMarkup(galleryItems);
+
+new SimpleLightbox('.gallery a', {
+	captionsData: 'alt',
+	captionDelay: 250,
+});
+
+function createMarkup(gallery) {
+	const markupString = galleryItems.map(({ preview, original, description }) => 
+`<li class="gallery__item">
+   <a class="gallery__link" href=${original}>
+      <img class="gallery__image" src=${preview} alt=${description} />
+   </a>
+</li>`).join('');
+	
+	refs.container.insertAdjacentHTML('beforeend', markupString);
+};
