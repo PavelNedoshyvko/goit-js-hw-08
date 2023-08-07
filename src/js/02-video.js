0eb5cc3374ba3ef1e496e19a3e6e9d48e03fde7f
Pavel Nedoshyvko <plight83@gmail.com>
2023-08-07 17:35:04 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -12,8 +12,8 @@ function recordingCurrentTime(data) {
 }
 
 try {
-	const videoplayerCurrentTime = JSON.parse(localStorage.getItem("videoplayer-current-time"));
-	player.setCurrentTime(videoplayerCurrentTime.seconds);
+	const {seconds} = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+	player.setCurrentTime(seconds);
 } catch (error) {
 	console.log(error.name);
 	console.log(error.message);
