e1b14175e0b1169fbbd177b6073885d83d445000
Pavel Nedoshyvko <plight83@gmail.com>
2023-08-14 13:46:03 +0300

    added 03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from "lodash.throttle";
 
+
+const formEl = document.querySelector('.feedback-form');
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onFormInput, 500));
+
+
+if (JSON.parse(localStorage.getItem("feedback-form-state"))) {
+	const formData = JSON.parse(localStorage.getItem("feedback-form-state"));
+	formEl.elements.email.value = formData.email;
+	formEl.elements.message.value = formData.message;
+}
+
+function onFormSubmit(evt) {
+	evt.preventDefault();
+	if (formEl.elements.email.value === '' || formEl.elements.message.value === '') {
+			return alert('All fields must be filled!!!')
+		};
+	console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+	localStorage.clear();
+	evt.currentTarget.reset();
+}
+
+function onFormInput(evt) {
+	const formData = {
+			email: formEl.elements.email.value,
+			message: formEl.elements.message.value,
+	};
+	localStorage.setItem("feedback-form-state", JSON.stringify(formData));	
+}
